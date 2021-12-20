99e635156857d6b7bc0496572793815b7e93d906
Brian Hudson <reallybrianhudson@gmail.com>
2021-12-19 21:35:09 -0500

    Adding a bunch of data output and working on Dockerfile

--- FlightTracker/main.py
+++ FlightTracker/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import requests
 from src.environs import *
 from dateutil import parser
@@ -14,34 +16,48 @@ headers = {
     "apikey": apikey,
 }
 
+airport_list = ['YYC', 'STN', 'SEA', 'NRT', 'VCE',  'SIN', 'KUL', 'BKK', 'MNL']
+home_airport = 'BOS'
 search_dates = []
-starting_at = '2022/03/03'
-weeks = 5
+start_date = '2022/03/03'
+weeks = 10
 # dest = [('BOS', 'YYC'), ('BOS', 'LHR'), ('BOS', 'NRT'), ('BOS', 'SEA')]
 dest = [('BOS', 'LHR'), ('BOS', 'STN')]
 max_stopovers = 1
+max_limit_returns = 3
+weeks_traveling = 2
 
 
 def draw_table(params):
+    fly_from = ""
+    fly_to = ""
+    city_from = ""
+    city_to = ""
     request_api = requests.get(url=endpoint, params=params, headers=headers)
     request_api.raise_for_status()
     api_response_json = request_api.json()
     response_slice = api_response_json['data']
+    try:
+        fly_from = response_slice[0]['flyFrom']
+        fly_to = response_slice[0]['flyTo']
+        city_from = response_slice[0]['cityFrom']
+        city_to = response_slice[0]['cityTo']
+    except IndexError as error:
+        print("----- Error fetching data. Please verify parameters and try again.-----")
 
-    city_from = response_slice[0]['flyFrom']
-    city_to = response_slice[0]['flyTo']
-    table = Table(f"From {response_slice[0]['cityFrom']} to {response_slice[0]['cityTo']}",
-                  title_justify="left", box=box.MINIMAL_HEAVY_HEAD)
+    table = Table(f"From {city_from} to {city_to}",
+                  title_justify="center", box=box.MINIMAL_HEAVY_HEAD)
     console = Console()
     table.add_column("Price (USD)", style="bright_red", justify="right")
     table.add_column("Bag Price", justify="right")
-    table.add_column(f"Depart: {city_from}", style="green", justify="right")
-    table.add_column(f"Arrive: {city_to}", style="yellow", justify="right")
+    table.add_column(f"Depart: {fly_from}", style="green", justify="right")
+    table.add_column(f"Arrive: {fly_to}", style="yellow", justify="right")
     table.add_column("Duration", justify="right")
     table.add_column("Conn.", justify="right")
     table.add_column("Dist. (mi)", justify="right")
     table.add_column("Airline", justify="right")
     table.add_column("Open Seats", justify="right")
+    table.add_column("URL", justify="right")
 
     for index in range(len(response_slice)):
         route_list = response_slice[index]['route']
@@ -50,8 +66,8 @@ def draw_table(params):
             if route_count >= 1:
                 route_list_string += " | "
             route_list_string += f"{route_list[route_count]['flyFrom']} -> {route_list[route_count]['flyTo']}"
-        price = f"{response_slice[index]['price']}.00"
-        bag_price = f"{response_slice[index]['bags_price'].pop('1')}"
+        price = "{:,.2f}".format(response_slice[index]['price'])
+        bag_price = "{:,.2f}".format(response_slice[index]['bags_price'].pop('1'))
         departure = parser.parse(response_slice[index]['local_departure']).strftime('%m/%d at %H:%m')
         arrival = parser.parse(response_slice[index]['local_arrival']).strftime('%m/%d at %H:%m')
         duration_seconds = response_slice[index]['duration']['total']
@@ -60,39 +76,48 @@ def draw_table(params):
         distance = f"{response_slice[index]['distance']}"
         airline = f"{response_slice[index]['airlines'][0]}"
         seats_available = f"{response_slice[index]['availability']['seats']}"
+        deep_link = f"[link={response_slice[index]['deep_link']}]Link[/link]"
         table.add_row(f"{route_list_string}", price, bag_price, departure, arrival,
-                      duration_hms, connections, distance, airline, seats_available)
+                      duration_hms, connections, distance, airline, seats_available, deep_link)
     console.print(table)
 
 
-for index in range(weeks):
-    start_date = datetime.strptime(starting_at, '%Y/%m/%d')
-    search_date_start = (start_date + timedelta(weeks=index))
+def get_flights(starting_at, from_airport, to_airport, span, currency, cabin):
+    start_at = datetime.strptime(starting_at, '%Y/%m/%d')
+    search_date_start = (start_at + timedelta(weeks=span))
     search_date_start_string = search_date_start.strftime('%d/%m/%Y')
     search_date_end = (search_date_start + timedelta(days=2)).strftime('%d/%m/%Y')
-    return_date_start = (start_date + timedelta(weeks=index + 2))
+    return_date_start = (start_at + timedelta(weeks=span + 2))
     return_date_start_string = return_date_start.strftime('%d/%m/%Y')
     return_date_end = (return_date_start + timedelta(days=2)).strftime('%d/%m/%Y')
-    for d in dest:
-        flight_params = {
-            "fly_from": f"{d[0]}",
-            "fly_to": f"{d[1]}",
-            "date_from": f"{search_date_start_string}",
-            "date_to": f"{search_date_end}",
-            "curr": "USD",
-            "selected_cabins": "M",
-            "max_stopovers": max_stopovers,
-            "limit": 5
-        }
-        draw_table(flight_params)
-        flight_params = {
-            "fly_from": f"{d[1]}",
-            "fly_to": f"{d[0]}",
-            "date_from": f"{return_date_start_string}",
-            "date_to": f"{return_date_end}",
-            "curr": "USD",
-            "selected_cabins": "M",
-            "max_stopovers": max_stopovers,
-            "limit": 5
-        }
-        draw_table(flight_params)
+    flight_params = {
+        "fly_from": f"{from_airport}",
+        "fly_to": f"{to_airport}",
+        "date_from": f"{search_date_start_string}",
+        "date_to": f"{search_date_end}",
+        "curr": f"{currency}",
+        "selected_cabins": f"{cabin}",
+        "max_stopovers": max_stopovers,
+        "limit": max_limit_returns,
+        # "flight_type": "round"
+    }
+    draw_table(flight_params)
+    flight_params = {
+        "fly_from": f"{from_airport}",
+        "fly_to": f"{to_airport}",
+        "date_from": f"{return_date_start_string}",
+        "date_to": f"{return_date_end}",
+        "curr": f"{currency}",
+        "selected_cabins": f"{cabin}",
+        "max_stopovers": max_stopovers,
+        "limit": max_limit_returns,
+        # "flight_type": "round"
+    }
+    draw_table(flight_params)
+
+
+for i in airport_list:
+    get_flights(starting_at=start_date, from_airport=home_airport, to_airport=i,
+                span=weeks_traveling, currency='USD', cabin='M')
+    get_flights(starting_at=start_date, from_airport=i, to_airport=home_airport,
+                span=weeks_traveling, currency='USD', cabin='M')
